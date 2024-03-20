1b94e743a2a5eb9944d541bc0f6baab23fd846e8
MeetSuthar278 <161619636+MeetSuthar278@users.noreply.github.com>
2024-03-20 13:13:35 +0530

    Update ReservationFragment.java

--- app/src/main/java/com/example/myrail/ReservationFragment.java
+++ app/src/main/java/com/example/myrail/ReservationFragment.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class ResevationFragment extends Fragment {
+public class ReservationFragment extends Fragment {
 
 
     Button irctc;
